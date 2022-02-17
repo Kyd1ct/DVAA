9ac09683e1fa257ea6157b422552a482bd12ce83
Jack Gordon <35040964+jackiebiyy@users.noreply.github.com>
2022-02-17 11:01:58 +0000

    Updated App.js
    Added a button with an alert

--- DVAA/App.js
+++ DVAA/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Example</Text>
+      <Text>Open up App.js to start working on your app! asd</Text>
+      <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
       <StatusBar style="auto" />
     </View>
   );
