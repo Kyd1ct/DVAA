3d5738c96d42836ee1d443340f81d86b0c390a0e
Martin Georgiev <mgeorgiev5600@gmail.com>
2022-02-24 11:01:29 +0000

    Updates
    Flex box prototype
    Bottom navbar files

--- DVAA/App.js
+++ DVAA/App.js
@@ -1,22 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, Text, View, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! as</Text>
-      <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <View style={styles.container_main}>
+      <View style= {{flex:1}}>
+        <Text style ={styles.titleText}>Damn Vulnerable Android Application</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style = {{flex: 2}}>
+        <Text style={styles.normalText}>This is an intentionally vulnerable android application used to train developers and security professionals.</Text>
+      </View>
+      <View style = {{flex: 3}}>
+        <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
+      </View>
+    </View>
+
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  container_main: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 65 : 0
   },
+
+  titleText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
+  normalText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+
+
+  button_allign: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
