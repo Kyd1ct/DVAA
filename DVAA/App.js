39b94b7eb7f8b297ddfa5beac85a817dcfe10617
Martin Georgiev <mgeorgiev5600@gmail.com>
2022-02-24 12:04:01 +0000

    navbar fix

--- DVAA/App.js
+++ DVAA/App.js
@@ -1,42 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Button, Text, View, Alert } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MainContainer from './navigation/MainNavigation';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container_main}>
-      <View style= {{flex:1}}>
-        <Text style ={styles.titleText}>Damn Vulnerable Android Application</Text>
-        <StatusBar style="auto" />
-      </View>
-      <View style = {{flex: 2}}>
-        <Text style={styles.normalText}>This is an intentionally vulnerable android application used to train developers and security professionals.</Text>
-      </View>
-      <View style = {{flex: 3}}>
-        <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
-      </View>
-    </View>
-
+    // <View style={styles.container_main}>
+    //   <View style= {{flex:1}}>
+    //     <Text style ={styles.titleText}>Damn Vulnerable Android Application</Text>
+    //     <StatusBar style="auto" />
+    //   </View>
+    //   <View style = {{flex: 2}}>
+    //     <Text style={styles.normalText}>This is an intentionally vulnerable android application used to train developers and security professionals.</Text>
+    //   </View>
+    //   <View style = {{flex: 3}}>
+    //     <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
+    //   </View>
+    // </View>
+    <MainContainer/>
+  
   );
 }
 
---
+++ DVAA/navigation/mainnavigation.js
@@ -0,0 +1,100 @@
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { StyleSheet, Button, Text, View, Alert } from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+
+import HomeScreen from './screens/HomeScreen';
+import VulnScreen from './screens/VulnerabilitiesScreen';
+import DocScreen from './screens/DocumentationScreen';
+
+// Screen names
+const homeName = "Home";
+const vulnName = "Vulnerabilities";
+const docName = "Documentation";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer(){
+    return(
+        <NavigationContainer>
+        {/* <View style= {{flex:1}}>
+        <Text style ={styles.titleText}>Damn Vulnerable Android Application</Text>
+            <StatusBar style="auto" />
+        </View>
+        <View style = {{flex: 2}}>
+            <Text style={styles.normalText}>This is an intentionally vulnerable android application used to train developers and security professionals.</Text>
+        </View>
+        <View style = {{flex: 3}}>
+            <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
+        </View> */}
+            <Tab.Navigator 
+            initialRouteName={homeName}
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    let rn = route.name;
+
+                    if (rn === homeName) {
+                        iconName = focused ? 'home' : 'home-outline';
+                    }
+                    else if (rn === vulnName) {
+                        iconName = focused ? 'list' : 'list-outline';
+                    }
+                    else if (rn === docName) {
+                        iconName = focused ? 'settings' : 'settings-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color}/>
+                },
+            })}
+
+            tabBarOptions={{
+                activeTintColor: 'blue',
+                inactiveTintColor: 'grey',
+                labelStyle: {paddingBottom: 10, fontSize: 10},
+                style: {padding: 10, height: 70}
+            }}
+            >
+            
+                <Tab.Screen name={homeName} component={HomeScreen}/>
+                <Tab.Screen name={vulnName} component={VulnScreen}/>
+                <Tab.Screen name={docName} component={DocScreen}/>
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+const styles = StyleSheet.create({
+    container_main: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      paddingTop: Platform.OS === 'android' ? 65 : 0
+    },
+  
+    titleText: {
+      textAlign: 'center',
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
+  
+    normalText: {
+      textAlign: 'center',
+      fontSize: 20,
+    },
+  
+  
+    button_allign: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+  });
+  
