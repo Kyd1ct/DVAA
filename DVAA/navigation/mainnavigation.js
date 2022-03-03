3f2f7f713fb6d6cdbf9f41cd6aa40d26526da6ba
Martin Georgiev <mgeorgiev5600@gmail.com>
2022-03-03 10:17:02 +0000

    Updates to main screen
    Needs more work on the design and content alignment

--- DVAA/navigation/mainnavigation.js
+++ DVAA/navigation/mainnavigation.js
@@ -22,16 +22,6 @@ const Tab = createBottomTabNavigator();
 export default function MainContainer(){
     return(
         <NavigationContainer>
-        {/* <View style= {{flex:1}}>
-        <Text style ={styles.titleText}>Damn Vulnerable Android Application</Text>
-            <StatusBar style="auto" />
-        </View>
-        <View style = {{flex: 2}}>
-            <Text style={styles.normalText}>This is an intentionally vulnerable android application used to train developers and security professionals.</Text>
-        </View>
-        <View style = {{flex: 3}}>
-            <Button onPress={() => Alert.alert('simple button')} title="Example Button" color="#841584" accessibilityLabel='Test accessiblity text'/>
-        </View> */}
             <Tab.Navigator 
             initialRouteName={homeName}
             screenOptions={({route}) => ({
--- DVAA/navigation/screens/homeScreen.js
+++ DVAA/navigation/screens/homeScreen.js
@@ -1,14 +1,49 @@
 import * as React from 'react';
-import {View, Text } from 'react-native';
+import {View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 export default function HomeScreen(navigation) {
     return(
-    <View style ={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text
-         onPress={() => alert('This is the Home Screen.')}
-         style = {{fontSize: 26, fontWeight: 'bold'}}>
-                Home Screen
+    <View style = {{flex: 1, flexDirection: "column", paddingTop: 15}}>
+        <View style = {{flex : 1}}>
+        <Text style = {styles.titleText}>
+                Damn Vulnerable Android Application
         </Text>
+        </View>
+        <View style = {{flex: 2}}>
+            <Text style = {styles.normalText}>
+                This is vulnerable Android application created to aid developers and security researchers in writing secure code and improving their pentesting abilities.
+            </Text>
+        </View>
+        <View style = {{flex: 3, paddingLeft: 15, paddingRight: 15, alignItems: "center", flexDirection: 'row'}}>
+              <Button onPress={() => Alert.alert('About us')} title="About us" color="#841584" accessibilityLabel='Test accessiblity text'/>
+              <Button onPress={() => Alert.alert('More Info')} title="More information" color="#841584" accessibilityLabel='Test accessiblity text'/>
+        </View>
     </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container_main: {
+      backgroundColor: '#fff',
+      //paddingTop: Platform.OS === 'android' ? 65 : 0
+    },
+  
+    titleText: {
+      textAlign: 'center',
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
+  
+    normalText: {
+      textAlign: 'center',
+      fontSize: 20,
+    },
+  
+  
+    button_allign: {
+      flex: 1,
+      flexDirection: "row",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+  });
