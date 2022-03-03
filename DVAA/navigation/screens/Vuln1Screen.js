fac5d2d5786d823a02bab0408ee5939cc4a1225e
5384exe <szfaagier@gmail.com>
2022-03-03 10:41:11 +0000

    Added Vuln screens for each vulnerabilities,

---
+++ DVAA/navigation/screens/Vuln1Screen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text } from 'react-native';
+
+export default function Vuln1Screen(navigation) {
+    return(
+    <View style ={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text
+         onPress={() => alert('This is the Home Screen.')}
+         style = {{fontSize: 26, fontWeight: 'bold'}}>
+                Vulnerabilitie 1 Screen
+        </Text>
+    </View>
+    );
+}
---
+++ DVAA/navigation/screens/Vuln3Screen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {View, Text } from 'react-native';
+
+export default function Vuln3Screen(navigation) {
+    return(
+    <View style ={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text
+         onPress={() => alert('This is the Home Screen.')}
+         style = {{fontSize: 26, fontWeight: 'bold'}}>
+                Vulnerabilitie 3 Screen
+        </Text>
+    </View>
+    );
+}
